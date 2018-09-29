76f9591dfce31d9425b1a897bc18536287da2269
kokeshing <kou7cdb1a@gmail.com>
2018-09-29 17:54:36 +0900

    fix

--- bunny/void.py
+++ bunny/void.py
@@ -154,7 +154,7 @@ Please use `tqdm_gui(...)` instead of `tqdm(..., gui=True)`
                 tqdm.write(offset + f'|   {obj:>6}       |')
                 tqdm.write(offset + '| ＿＿＿_＿＿＿＿|')
                 tqdm.write(offset + '        ||')
-                tqdm.write(offset + '(´･_･`)|| ')
+                tqdm.write(offset + ' (´･_･`)|| ')
                 tqdm.write(offset + ' / 　 づ')
 
             # Closing the progress bar.
